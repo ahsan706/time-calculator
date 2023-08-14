bbacaaa8af6ddc13293c192bb010a377e7307223
Ahsan <ahsan.703@gmail.com>
2023-08-14 21:20:36 +0200

    fix empty text issue

--- src/timeCalculator/util/utils.ts
+++ src/timeCalculator/util/utils.ts
@@ -9,12 +9,16 @@ export function getNumberOfDaysBetweenDates(startDate: Date | null, endDate: Dat
     }
   }
   export function getFormatedStringFromDays(numberOfDays:number) {
-    var years = Math.floor(numberOfDays / 365);
-    var months = Math.floor(numberOfDays % 365 / 30);
-    var days = Math.floor(numberOfDays % 365 % 30);
+    if(numberOfDays === 0){
+        return "0 day";
+    }else{
+        var years = Math.floor(numberOfDays / 365);
+        var months = Math.floor(numberOfDays % 365 / 30);
+        var days = Math.floor(numberOfDays % 365 % 30);
 
-    var yearsDisplay = years > 0 ? years + (years == 1 ? " year, " : " years, ") : "";
-    var monthsDisplay = months > 0 ? months + (months == 1 ? " month, " : " months, ") : "";
-    var daysDisplay = days > 0 ? days + (days == 1 ? " day" : " days") : "";
-    return yearsDisplay + monthsDisplay + daysDisplay; 
+        var yearsDisplay = years > 0 ? years + (years === 1 ? " year, " : " years, ") : "";
+        var monthsDisplay = months > 0 ? months + (months === 1 ? " month, " : " months, ") : "";
+        var daysDisplay = days > 0 ? days + (days === 1 ? " day" : " days") : "";
+        return yearsDisplay + monthsDisplay + daysDisplay; 
+    }
 }
