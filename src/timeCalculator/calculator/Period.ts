1f5256920e85b029f40a3edf318ffd8a2891a1e9
Ahsan <ahsan.703@gmail.com>
2023-08-15 11:46:50 +0200

    fix param

--- src/timeCalculator/calculator/Period.ts
+++ src/timeCalculator/calculator/Period.ts
@@ -1,23 +1,28 @@
 import { getNumberOfDaysBetweenDates } from "../util/utils";
 
 export class Period {
+  name: string;
+  id: string;
+  startDate: Date | null;
+  endDate: Date | null;
+
+  constructor({
+    name,
+    id,
+    startDate,
+    endDate,
+  }: {
     name: string;
     id: string;
     startDate: Date | null;
     endDate: Date | null;
-
-    constructor({ name, id, startDate, endDate }: {
-        name: string,
-        id: string,
-        startDate: Date | null,
-        endDate: Date |null
-    }) {
-        this.name = name;
-        this.id = id;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-    get days() {
-        return getNumberOfDaysBetweenDates(this.endDate, this.startDate);
-    }
+  }) {
+    this.name = name;
+    this.id = id;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+  get days() {
+    return getNumberOfDaysBetweenDates(this.startDate, this.endDate);
+  }
 }
